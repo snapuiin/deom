cb133e59e037a479d3aa3aa42abc034003f06f3f
snapuiin <abdulla07cr7@gmail.com>
2025-06-22 08:25:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+// let pass ;
+// if (pass   ===  8){
+//     console.log("welcom");
+
+// }else if (pass <= 8){
+//     console.log("passwprdis to short ");
+// }else if (pass >= 8){
+//     console.log("pass is to long ");
+
+// }else {
+//     console.log("pls pass");
+// }
+// let fruit = "oo";
+// switch(fruit){
+//     case  "banana":
+//     console.log("bna is good ");
+//     break;
+//     case "orange":
+//         console.log("im fan");
+//         break;
+//         case "apple":
+//             console.log("apple is goofd");
+//             break;
+//             default:
+//                 console.log("ga");
+   
+
+// for ( let i = 0; i<=1000; i++){
+    // console.log("and",i);
+// }
+// let i = 10;
+// while (i<=100){
+//     console.log("hei",i);
+
+//     i++;
+// }
+// let i = 10;
+// while (i<= 100){
+//     console.log("hi",i);
+//     i++;
+// }
+// let i =0;
+// do{
+//     console.log("abd",i);
+//     i++;
+// }while(i<=100)
+// let i = 0;
+// do{
+//     console.log("hi",i);
+//     i++;
+// // }while(i<=100)
+// let favsin = ["encore","seedhemaut","calm"]
+// console.log(favsin[0]);
+// let favnum =["7","21","15","30"]
+// // console.log(favnum[1]);
+// let mix = ["string",["otherarr"],[123],true];
+// console.log(mix);
+// let car = {
+//     type : "bmw",
+//     model:"m5",
+//     color:"black"
+// }
+// console.log(car);
+// console.log(car.type = "toy");
+// console.log(car);
+// car.weels ="5"
+// console.log(ca);
+// function myfun (p1 , p2){
+//     return p1 * p2;
+
+
+// }let x = myfun(4,5);
+// console.log({x);
+// function myf (p1 , p2){
+//     return p1 + p2;
+
+// }let lal =(9+1)
+// console.log(lal);
